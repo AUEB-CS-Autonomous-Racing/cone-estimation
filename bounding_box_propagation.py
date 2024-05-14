5adefab03737a85427933d70fd99c3e674c31de5
fanisvl <fanis.vlahogiannis@gmail.com>
2024-05-14 21:03:44 +0300

    refactor for clarity, add rotation vector comment

--- bounding_box_propagation.py
+++ bounding_box_propagation.py
@@ -7,49 +7,6 @@ from pnp_algorithm import pnp
 import numpy as np
 import matplotlib.pyplot as plt
 
-def keypoint_regression(image):
-    keypoint_model_src = 'models/E10-AVL9.9054.pth'
-    
-
-    device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-    )
-
-    # Load the saved model
-    model = cnn()  # Assuming 'cnn' is your model class
-    model.load_state_dict(torch.load(keypoint_model_src, map_location=torch.device(device)))
-    model.eval()  # Set model to evaluation mode
-
-
-    # Define the transform for preprocessing the images
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((80, 80)),  # Resize the image to match the input size of your model
-        transforms.ToTensor(),         # Convert the image to a PyTorch tensor
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalize the image
-    ])
-
-    # Apply the transform to preprocess the image
-    input_image = transform(image).unsqueeze(0)  # Add a batch dimension
-
-    # Move the input image to the device (GPU or CPU)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input_image = input_image.to(device)
-
-    # Perform inference
-    with torch.no_grad():  # Disable gradient calculation during inference
-        model.eval()  # Set model to evaluation mode
-        output = model(input_image)
-
-    # Extract keypoints from the output tensor
-    keypoints = output.squeeze().cpu().numpy()  # Assuming output is a tensor
-
-    return keypoints
-
 colors = {
     0: '#0000FF',
     1: '#FFFF00',
@@ -58,7 +15,7 @@ colors = {
 }
 
 def main():
-    image_path = 'full_images/amz_00001.jpg'
+    image_path = 'full_images/amz_00005.jpg'
     image = cv2.imread(image_path)
 
     cone_detection_src = 'models/yolov8s700.pt'
@@ -119,6 +76,10 @@ def main():
 
     # # Plot estimated cone position
     for cone in cone_positions:
+
+        # rotated_pos = np.dot(R, cone["pos"])
+        # cone["pos"] = rotated_pos
+
         plt.scatter(cone["pos"][0], cone["pos"][1], color=colors[cone['label']])
         plt.annotate(f"{cone["id"]}", (cone["pos"][0], cone["pos"][1]+3))
 
@@ -131,6 +92,50 @@ def main():
     plt.grid(True)
     plt.gca().set_aspect('equal', adjustable='box')
     plt.show()
+
+
+def keypoint_regression(image):
+    keypoint_model_src = 'models/E10-AVL9.9054.pth'
+    
+
+    device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+    )
+
+    # Load the saved model
+    model = cnn()  # Assuming 'cnn' is your model class
+    model.load_state_dict(torch.load(keypoint_model_src, map_location=torch.device(device)))
+    model.eval()  # Set model to evaluation mode
+
+
+    # Define the transform for preprocessing the images
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((80, 80)),  # Resize the image to match the input size of your model
+        transforms.ToTensor(),         # Convert the image to a PyTorch tensor
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalize the image
+    ])
+
+    # Apply the transform to preprocess the image
+    input_image = transform(image).unsqueeze(0)  # Add a batch dimension
+
+    # Move the input image to the device (GPU or CPU)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    input_image = input_image.to(device)
+
+    # Perform inference
+    with torch.no_grad():  # Disable gradient calculation during inference
+        model.eval()  # Set model to evaluation mode
+        output = model(input_image)
+
+    # Extract keypoints from the output tensor
+    keypoints = output.squeeze().cpu().numpy()  # Assuming output is a tensor
+
+    return keypoints
 
 if __name__ == '__main__':
     main()
