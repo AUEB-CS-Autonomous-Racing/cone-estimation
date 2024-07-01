2509e7ea03bc62806930cea7fb3d14e461909624
fanisvl <fanis.vlahogiannis@gmail.com>
2024-07-01 17:10:57 +0300

    keypoint reg batch eval working'

--- cone_estimation.py
+++ cone_estimation.py
@@ -17,6 +17,14 @@ colors = {
 }
 
 def main():
+    """"
+    > Full Img 
+    > Bounding Box Detection 
+    > Crop Cone Imgs to feed into keypoint regression model
+    > Keypoint Regression on Cropped Images (x,y output on cropped coordinates) 
+    > Translation to full image coordinates
+    > PnP
+    """
 
     total_time_start = time.time()
 
@@ -25,6 +33,7 @@ def main():
 
     cone_detection_src = 'models/yolov8s700.pt'
     cone_detection_model = YOLO(cone_detection_src)
+    keypoint_model = KeypointRegression('models/E10-AVL9.9054.pth')
 
     cone_det_start = time.time()
     result = cone_detection_model.predict(image)
@@ -33,59 +42,58 @@ def main():
 
     full_image = cv2.imread(image_path)
 
-    cone_positions = []
+    cone_info = {}
+    cropped_cones = []
     id = 0
-    
 
-    keypoint_model = KeypointRegression('models/E10-AVL9.9054.pth')
-    cropped_boxes = []
+    # Gather info for each cone and store in cone_info
+    # Crop cone images
     for box in bounding_boxes:
-        id += 1
-        conf = box.conf.item()
         label = box.cls.item()
         x1, y1, x2, y2 = map(int, box.xyxy[0])
+        cone_info[id] = {"id": id, "label": label, "x1": x1, "y1": y1, "x2": x2, "y2": y2}
         cropped_img = image[y1:y2, x1:x2]
-
-        cropped_height, cropped_width, _ = cropped_img.shape
-
-        cropped_boxes.append(cropped_img)
-        print("Cropped (width x height):", cropped_width, cropped_height, "\n")
+        cropped_cones.append(cropped_img)
+        id += 1
 
     keypoint_reg_start = time.time()
-    keypoints = keypoint_model.eval(cropped_boxes)
-    print(keypoints.shape)
+    keypoints = keypoint_model.eval(cropped_cones)
     keypoint_reg_end = time.time()
+    print("Keypoint regression finished")
+
+    print(keypoints.shape)
 
     keypoints_2d = {}
-    cv2.putText(full_image, str(id), (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 1)
+    # Map 7 keypoints for each cone
+    for cone in cone_info.values():
+    
+        cv2.putText(full_image, str(cone["id"]), (cone["x2"], cone["y2"]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 1)
 
-    # Map model keypoint output from cropped image to full image coordinates
-    for i in range(0, len(keypoints), 2):
+        # Map keypoint model  output from cropped image to full image coordinates
+        # The model output is 14 (x,y) coordinates => 7 keypoints
+        for point in range(0, len(keypoints[0]), 2):
+            cropped_height, cropped_width, _ = cropped_img.shape
+            cropped_height = cone["y2"] - cone["y1"]
+            cropped_width = cone["x2"] - cone["x1"]
 
-        # TODO: Does this work?
-        print(i)
-        cropped_img = cropped_boxes[int(i/2)] # for loop step = 2
-        cropped_height, cropped_width, _ = cropped_img.shape
+            cropped_x = int(keypoints[cone["id"]][point] / 100.0 * cropped_width)
+            cropped_y = int(keypoints[cone["id"]][point+1] / 100.0 * cropped_height)
 
-        cropped_x = int(keypoints[i] / 100.0 * cropped_width)
-        cropped_y = int(keypoints[i+1] / 100.0 * cropped_height)
+            full_x = int(cropped_x + cone["x1"])
+            full_y = int(cropped_y + cone["y1"])
 
-        full_x = int(cropped_x + x1)
-        full_y = int(cropped_y + y1)
+            # Add to 2D keypoints map for PnP
+            keypoints_2d[point//2] = [full_x, full_y]
 
-        # Add to 2D keypoints map for PnP
-        keypoints_2d[i//2] = [full_x, full_y]
+            cv2.circle(full_image, (full_x, full_y), 2, cone["label"], -1)
 
-        cv2.circle(full_image, (full_x, full_y), 2, (0, 0, 255), -1)
-
-        # Estimate cone position
-        print(f"id: {id}")
+        # Estimate cone position with PnP using all the 2d keypoints for this cone
         rvec, tvec = pnp(keypoints_2d)
         tvec = tvec / 10000
         tvec[2] /= 100
-        print(f"Translation Vector:\n{tvec}")
 
-        cone_positions.append({"id": id, "label": label, "pos": tvec})  
+        cone["pos"] = tvec
+    
 
     total_time_end = time.time()
     print(f"\nTotal Pipeline Time: {total_time_end-total_time_start:.4}")  
@@ -103,7 +111,7 @@ def main():
     plt.grid(True)
     plt.gca().set_aspect('equal', adjustable='box')
     
-    for cone in cone_positions:   
+    for cone in cone_info.values():   
         x = cone["pos"][0]
         z = cone["pos"][2]
         plt.scatter(x, z, color=colors[cone['label']])
