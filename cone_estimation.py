86a102e31f0b5f246903a72858d2ddb645804fe6
fanisvl <fanis.vlahogiannis@gmail.com>
2024-08-18 14:08:23 +0300

    Adjust for integrated bounding box and keypoint YOLO model

--- cone_estimation.py
+++ cone_estimation.py
@@ -7,7 +7,14 @@ import time
 import numpy as np
 import json
 
-colors = {
+COLORS_CV2 = {
+    0: (255, 0, 0),    # Blue  - #0000FF
+    1: (0, 255, 255),  # Yellow - #FFFF00
+    2: (0, 165, 255),  # Orange - #FFA500
+    3: (0, 165, 255),  # Orange - #FFA500
+}
+
+COLORS_HEX = {
     0: '#0000FF',
     1: '#FFFF00',
     2: '#FFA500',
@@ -29,100 +36,62 @@ def cone_estimation(image_path, demo=True):
     total_time_start = time.time()
     image = cv2.imread(image_path)
 
-    cone_detection_src = 'models/yolov8n.pt'
-    cone_detection_model = YOLO(cone_detection_src)
-    keypoint_model = KeypointRegression('models/E10-AVL9.9054.pth')
+    model_src = 'models/yolo-nano.pt'
+    cone_detection_model = YOLO(model_src)
+    print("Model loaded.")
 
-    cone_det_start = time.time()
-    result = cone_detection_model.predict(image)
-    cone_det_end = time.time()
-    bounding_boxes = result[0].boxes
+    image = cv2.imread(image_path)
 
-    full_image = cv2.imread(image_path)
+    results = cone_detection_model.predict(image)
+    results = results[0]
 
-    cone_estimates = {}
-    cropped_cones = []
-    id = 0
+    if demo:
+        for box in results.boxes:
+            x1, y1, x2, y2 = box.xyxy.numpy()[0]
+            conf = box.conf.item()
+            class_id = int(box.cls.item())
+            print([x1, y1, x2, y2], conf, class_id)
+            cv2.rectangle(image, (int(x1),int(y1)), (int(x2),int(y2)), COLORS_CV2[class_id], 1)
+        for cone in results.keypoints.data:
+            for (x,y) in cone:
+                cv2.circle(image, (int(x),int(y)), 1, (0, 0, 255), 1)
+        cv2.imshow("img", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    # Gather info for each cone and store in cone_estimates
-    # Crop cone images
-    for box in bounding_boxes:
-        label = box.cls.item() # 2 = orange, 1 = yellow, 0 = blue
-        x1, y1, x2, y2 = map(int, box.xyxy[0])
-        cone_estimates[id] = {"id": id, "label": label}
-        cropped_img = image[y1:y2, x1:x2]
-        cropped_cones.append(cropped_img)
-        id += 1
-
-    keypoint_reg_start = time.time()
-    keypoints = keypoint_model.eval(cropped_cones)
-    keypoint_reg_end = time.time()
-    print("Keypoint regression finished")
-
-    keypoints_2d = {}
-    # Map 7 keypoints for each cone
-    for cone in cone_estimates.values():
-        # Map keypoint model  output from cropped image to full image coordinates
-        # The model output is 14 (x,y) coordinates => 7 keypoints
-
-        id = cone["id"]
-        x1, y1, x2, y2 = map(int, bounding_boxes[id].xyxy[0]) # bounding box coordinates in full image
-        for point in range(0, len(keypoints[0]), 2):
-            cropped_height = y2 - y1
-            cropped_width = x2 - x1
-
-            cropped_x = int(keypoints[cone["id"]][point] / 100.0 * cropped_width)
-            cropped_y = int(keypoints[cone["id"]][point+1] / 100.0 * cropped_height)
-            
-            full_x = int(cropped_x + x1)
-            full_y = int(cropped_y + y1)
-
-            # Add to 2D keypoints map for PnP
-            keypoints_2d[point//2] = [full_x, full_y]
-
-            if demo:
-                cv2.putText(full_image, str(cone["id"]), (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 1)
-                cv2.circle(full_image, (full_x, full_y), 2, (0, 0, 255), -1)
-
-        # Estimate cone position with PnP using all the 2d keypoints for this cone
-        # TODO: Tune PnP output based on real world measurments
-        rvec, tvec = PnP(keypoints_2d)
+    cones = []
+    for cone_points in results.keypoints.data:
+        rvec, tvec = PnP(cone_points)
+        
+        # magic
         tvec /= 1000
         tvec[2] /= 100
 
-        # 2D Coordinates for cone position in map
-        cone["X"] = tvec[0][0]
-        cone["Y"] = tvec[2][0]
-    
+        x, y = tvec[0][0], tvec[2][0]
+        cones.append((x, y))
+
+    if demo:
+        x_coords, y_coords = zip(*cones)
+        plt.figure(figsize=(8, 6))
+        # Create a scatter plot
+        plt.scatter(0, 0, color='red', marker='x', s=100, edgecolor='black')
+        plt.scatter(x_coords, y_coords, color='blue', marker='o', s=100, edgecolor='black')
+
+        # Add labels and title
+        plt.xlabel('X-axis')
+        plt.ylabel('Y-axis')
+        plt.title('Scatter Plot of Points')
+
+        # Optionally, add grid
+        plt.grid(True)
+
+        # Display the plot
+        plt.show()
 
     total_time_end = time.time()
     print(f"\nTotal Pipeline Time: {total_time_end-total_time_start:.4}")  
-    print(f"Cone Detection Time: {cone_det_end-cone_det_start:.4}")
-    print(f"Total Keypoint Regr. Time: {keypoint_reg_end-keypoint_reg_start:.4}")
 
-    if demo:
-        cv2.imshow("Keypoints", full_image)
-        cv2.waitKey(0)
-        plt.title("Estimated Cone Position Relative to Camera")
-        plt.scatter(0, 0, color='r', label='Camera')
-        plt.xlabel("X-axis")
-        plt.ylabel("Depth-axis")
-        plt.legend()
-        plt.grid(True)
-        plt.gca().set_aspect('equal', adjustable='box')
-        plt.axis('equal')
-    
-        for cone in cone_estimates.values():   
-            x = cone["X"]
-            y = cone["Y"]
-            plt.scatter(x, y, color=colors[cone['label']])
-            plt.annotate(f'{cone["id"]}', (x, y))
-
-        plt.show()
-    
-    with open(f"json_estimates/{image_path.replace("full_images/", "").replace(".jpg", "")}.json", 'w+') as f:
-        json.dump(cone_estimates, f)
-    return cone_estimates
+    return cones
 
 if __name__ == '__main__':
     cone_estimation('full_images/00.jpg')
